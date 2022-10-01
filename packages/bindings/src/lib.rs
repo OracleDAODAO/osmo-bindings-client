510f57135a8c936629911e1501f8055f4a7fed39
Maurits Bos <mb.brains@gmail.com>
2022-10-01 15:08:47 -0500

    removed the omsosisd dependency

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -11,7 +11,7 @@ pub use query::{
 };
 pub use types::{Step, Swap, SwapAmount, SwapAmountWithLimit};
 
-// This is a signal, such that any contract that imports these helpers will only run on the
-// osmosis blockchain
-#[no_mangle]
-extern "C" fn requires_osmosis() {}
+// // This is a signal, such that any contract that imports these helpers will only run on the
+// // osmosis blockchain
+// #[no_mangle]
+// extern "C" fn requires_osmosis() {}
